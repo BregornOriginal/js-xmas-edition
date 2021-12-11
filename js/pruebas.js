152dc3fa611139a4f477afb9c1b16fdd0353f93d
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2021-12-11 16:21:20 -0300

    Se completó tarea clase 7.
    Se agregaron funciones de validación para los siguientes campos: Nombre, Ciudad, Descripción Regalo

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,7 +1,7 @@
 function probarValidarNombre() {
   console.assert(
       validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
+      'Validar nombre no validó que el nombre no sea vacío'
   );
 
   console.assert(
@@ -13,3 +13,28 @@ function probarValidarNombre() {
 }
 
 probarValidarNombre();
+
+function probarValidarCiudad() {
+  console.assert(
+    validarCiudad('') === 'Seleccione la ciudad donde vive',
+    'Validar ciudad no validó que una ciudad este seleccionada'
+  );
+}
+
+probarValidarCiudad();
+
+function probarValidarDescripcionRegalo(){
+  console.assert(
+    validarDescripcionRegalo('') === 'Escriba que regalo le gustaría recibir',
+  'Validar descripción regalo no valido que se haya completado el campo de texto'
+  );
+
+  console.assert(
+    validarDescripcionRegalo('11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111')
+    === 'Este campo debe tener menos de 100 caracteres',
+    'Validar descripción regalo debe tener menos de 100 caracteres'
+  );
+ }
+
+ probarValidarDescripcionRegalo();
+

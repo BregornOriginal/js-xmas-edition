1f31a52359e999175fa58c09c0086d22cfce71a9
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2021-12-12 22:21:30 -0300

    Se agregaron validaciones positivas
    Se agregaron validaciones de casos positivos para las funciones de pruebas.

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,40 +1,57 @@
 function probarValidarNombre() {
+
   console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío'
+    validarNombre("") === "Este campo debe tener al menos 1 caracter",
+    "Validar nombre no validó que el nombre no sea vacío"
   );
 
   console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+    validarNombre(
+      "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+    ) === "Este campo debe tener menos de 50 caracteres",
+    "Validar nombre no validó que el nombre sea menor a 50 caracteres"
+  );
+
+  console.assert(
+    validarNombre("Julio") === "",
+    "validarNombre falló con un nombre válido"
   );
 }
 
 probarValidarNombre();
 
 function probarValidarCiudad() {
-  console.assert(
-    validarCiudad('') === 'Seleccione la ciudad donde vive',
-    'Validar ciudad no validó que una ciudad este seleccionada'
+
+  console.assert(validarCiudad("") === "Seleccione la ciudad donde vive",
+    "Validar ciudad no validó que una ciudad este seleccionada"
   );
 }
 
+console.assert(
+  validarCiudad("Buenos Aires") === "",
+  "Validar ciudad no funcionó con un nombre de ciudad válido"
+);
+
 probarValidarCiudad();
 
-function probarValidarDescripcionRegalo(){
+function probarValidarDescripcionRegalo() {
+  
   console.assert(
-    validarDescripcionRegalo('') === 'Escriba que regalo le gustaría recibir',
-  'Validar descripción regalo no valido que se haya completado el campo de texto'
+    validarDescripcionRegalo("") === "Escriba que regalo le gustaría recibir",
+    "Validar descripción regalo no valido que se haya completado el campo de texto"
   );
 
   console.assert(
-    validarDescripcionRegalo('11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111')
-    === 'Este campo debe tener menos de 100 caracteres',
-    'Validar descripción regalo debe tener menos de 100 caracteres'
+    validarDescripcionRegalo(
+      "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+    ) === "Este campo debe tener menos de 100 caracteres",
+    "Validar descripción regalo debe tener menos de 100 caracteres"
   );
- }
 
- probarValidarDescripcionRegalo();
+  console.assert(validarDescripcionRegalo("Regalo") === "",
+    "La función validar descripción regalo no funcionó con una descripción correcta"
+  );
+}
+
+probarValidarDescripcionRegalo();
 

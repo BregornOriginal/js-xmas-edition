cb9cf79639111e89e8fdec44a66490b414d7dc14
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2021-12-17 00:16:12 -0300

    Se agregaron calculos
    Se separaron en archivos las pruebas y los calculos.

---
+++ clase-6/tarea-1-calculos.js
@@ -0,0 +1,29 @@
+function calculateOlderAge(arrayAge) {
+    let maximumValue = arrayAge[0];
+    for (i = 0; i < arrayAge.length; i++) {
+      if (arrayAge[i] > maximumValue) {
+        maximumValue = arrayAge[i];
+      }
+    }
+    return maximumValue;
+  }
+  
+  function calculateYoungerAge(arrayAge) {
+    let minimunValue = arrayAge[0];
+    for (i = 0; i < arrayAge.length; i++) {
+      if (arrayAge[i] < minimunValue) {
+        minimunValue = arrayAge[i];
+      }
+    }
+    return minimunValue;
+  }
+  
+  function calculateAverageAge(arrayAge) {
+    let averageAge = 0;
+    let result = 0;
+    for (i = 0; i < arrayAge.length; i++) {
+      result += +arrayAge[i];
+      averageAge = result / arrayAge.length;
+    }
+    return averageAge;
+  }

9ca46da5db8b94c8c881e33ed13582ab967b341b
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2021-12-17 00:26:58 -0300

    Changed language
    On this step the language was changed from Spanish to English.

--- clase-6/tarea-1.js
+++ clase-6/tarea-1.js
@@ -1,70 +1,60 @@
 /*
-TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
+TAREA-1 CLASE 6: Empezar preguntando cuánta gente hay en el grupo familiar.
 Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
-Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
+Al hacer click en "calculate", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
 
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, 
 borrando los inputs ya creados (investigar cómo en MDN).
 */
 
-// según la respuesta que reciba del prompt voy a tener que crear tantos nodos inputs y labels.
-// colocar en el input la edad.
-// tengo que crear un botón para calcular.
-// ese botón me tiene que mostrar en un elemento <em> u otro a elección la persona de mayor edad,
-// de menor edad y el promedio de edad del grupo familiar.
-// también puedo crear un botón que al utilizarlo me limpie los inputs ya creados.
-
-// Se entregó la tarea con los 4 botones, en este código se encuentra activo solo el botón
-// mayor edad que cumple con todos los cálculos solicitados.
-
-const $formulariosGrupoFamiliar = document.querySelector(
-  "#formularios-grupo-familiar"
+const $familaryGroupFormulary = document.querySelector(
+  "#familary-group-formulary"
 );
 
-document.querySelector("#calcular").className = "oculto";
+document.querySelector("#calculate").className = "hidden";
 
-const $limpiarFormulario = document.querySelector("#limpiar");
+const $cleanFormulary = document.querySelector("#clean");
 
-$limpiarFormulario.onclick = function (event) {
-  const $resultado = document.querySelector("#resultado");
-  let $padre = document.getElementById("contenedor-integrantes");
-  let $inputPrincipal = document.getElementById("cantidad-integrantes");
+$cleanFormulary.onclick = function (event) {
+  const $result = document.querySelector("#result");
+  let $pather = document.getElementById("container-members");
+  let $principalInput = document.getElementById("number-members");
   
-  $inputPrincipal.value = "";
-  $resultado.innerText = "";
-  $padre.innerHTML = "";
-  document.querySelector("#calcular").className = "oculto";
-  document.querySelector("#enviar-datos").className = "visible";
+  $principalInput.value = "";
+  $result.innerText = "";
+  $pather.innerHTML = "";
+  document.querySelector("#calculate").className = "hidden";
+  document.querySelector("#send-data").className = "visible";
   
   event.preventDefault();
 };
 
-const $botonEnviar = document.querySelector("#enviar-datos");
+const $sendButton = document.querySelector("#send-data");
 
-$botonEnviar.onclick = function (event) {
+$sendButton.onclick = function (event) {
   
-  document.querySelector("#enviar-datos").className = "oculto";
-  document.querySelector("#calcular").className = "visible";
+  document.querySelector("#send-data").className = "hidden";
+  document.querySelector("#calculate").className = "visible";
 
-  const $ContenedorIntegrantes = document.querySelector("#contenedor-integrantes");
-  const $inputIntegrantes = document.querySelector("#cantidad-integrantes");
-  const cantidadDeIntegrantes = Number($inputIntegrantes.value);
+  const $containerMembers = document.querySelector("#container-members");
+  const $inputMembers = document.querySelector("#number-members");
+  const numberOfMembers = Number($inputMembers.value);
 
-  if (cantidadDeIntegrantes > 0) {
+  if (numberOfMembers > 0) {
 
-    for (let i = 1; i <= cantidadDeIntegrantes; i++) {
-      const $nuevoInput = document.createElement("input");
-      const $nuevoLabel = document.createElement("label");
+    for (let i = 1; i <= numberOfMembers; i++) {
+      const $newInputMember = document.createElement("input");
+      const $NewLabelMember = document.createElement("label");
 
-      $nuevoInput.setAttribute("type", "number");
-      $nuevoInput.setAttribute("id", "valores");
-      $nuevoInput.setAttribute("placeholder", "Edad");
-      $nuevoInput.className = "input-clase";
+      $newInputMember.setAttribute("type", "number");
+      $newInputMember.setAttribute("id", "values");
+      $newInputMember.setAttribute("placeholder", "Age");
+      $newInputMember.className = "input-clase";
 
-      $nuevoLabel.innerText = `Integrante ${i}`;
-      $nuevoLabel.appendChild($nuevoInput);
+      $NewLabelMember.innerText = `Member # ${i}`;
+      $NewLabelMember.appendChild($newInputMember);
 
-      $ContenedorIntegrantes.appendChild($nuevoLabel);
+      $containerMembers.appendChild($NewLabelMember);
     }
   } else {
     
@@ -73,43 +63,51 @@ $botonEnviar.onclick = function (event) {
 };
 
 
-document.querySelector("#calcular").onclick = function (event) {
-  let arrayEdad = [];
-  let edadInputs = document.querySelectorAll("#valores");
-  const $resultado = document.querySelector("#resultado");
+document.querySelector("#calculate").onclick = function (event) {
+  let arrayAge = [];
+  let ageInputs = document.querySelectorAll("#values");
+  const $result = document.querySelector("#result");
 
-  for (let i = 0; i < edadInputs.length; i++) {
-    arrayEdad.push(Number(edadInputs[i].value));
+  for (let i = 0; i < ageInputs.length; i++) {
+    arrayAge.push(Number(ageInputs[i].value));
   }
 
-  let menorEdad = calcularMenorEdad(arrayEdad);
-  let mayorEdad = calcularMayorEdad(arrayEdad);
-  let promedioEdad = calcularPromedioEdad(arrayEdad);
+  let youngerAge = calculateYoungerAge(arrayAge);
+  let olderAge = calculateOlderAge(arrayAge);
+  let averageAge = calculateAverageAge(arrayAge);
 
-  $resultado.innerText = `El integrante con mayor edad tiene ${mayorEdad} años. El integrante con menor edad tiene ${menorEdad} años. El promedio de edad es de ${promedioEdad} años.`;
+  $result.innerText = `The oldest member is ${olderAge} years old. The youngest member is ${youngerAge} years old. The average age of the members is ${averageAge} years.`;
 
   event.preventDefault();
 };
 
-const $cantidadIntegrantes = document.querySelector("#cantidad-integrantes").value;
+//  TAREA CLASE 8
+// A las 2 tareas de la clase 6, ponerles las validaciones que consideren
+// necesarias (usando RegExp, Objetos, forEach, poner estilos, mostrar los errores
+//     en la interfaz de usuario y escribir pruebas).
+
+// TIP: Las edades no pueden tener decimales.
+//
+
+const $numberOfMembers = document.querySelector("#number-members").value;
 
 
 function validarIntegrantes (){
-  if ($cantidadIntegrantes <= 0){
-    "Este campo debe ser mayor al número 0"
+  if ($numberOfMembers <= 0){
+    "This field must be greater than the number 0"
   }
-  if ($cantidadIntegrantes === ""){
-    "Este campo debe contener al menos un número"
+  if ($numberOfMembers === ""){
+    "This field must have one number at least"
   }
-  if (!/^[0-9]+$/.test($cantidadIntegrantes)) {
-    "Este campo solo acepta números"
+  if (!/^[0-9]+$/.test($numberOfMembers)) {
+    "This field only accept numbers"
   }
   return "";
 };
 
-function probarError(){
-  const $errores = document.querySelector("#errores");
+function tryError(){
+  const $errors = document.querySelector("#errors");
   const $error = document.createElement("li");
-  $error.innerText = $errores;
-  $errores.appendChild($error);
+  $error.innerText = $errors;
+  $errors.appendChild($error);
 }

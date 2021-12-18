93e9fbd4fa97949c51524b948fb56030b0fc8e15
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2021-12-18 19:22:41 -0300

    Added validate and test error
    Have to fix the button Send, it's dosn't show the true errors. Every time i click it its show the same error

--- clase-6/tarea-1-pruebas.js
+++ clase-6/tarea-1-pruebas.js
@@ -1,16 +1,22 @@
-function testValidateIntegrantes() {
+function testValidateAmountOfMembers() {
   console.assert(
-    validateMembers("") === "This field must be greater than the number 0",
+    validateAmountOfMembers("") === "This field must have one number at least",
     "Validate members did not validate that members are not empty"
   );
   console.assert(
-    validateMembers(-2) === "This field must have one number at least",
+    validateAmountOfMembers(-2) ===
+      "This field must be greater than the number 0",
     "Validate members did not validate that members is a number greater than 0"
   );
+  
   console.assert(
-      validateMembers("fda") === "This field only accept numbers",
-      "Validate members did not validate that members contain numbers"
+    validateAmountOfMembers("fda") === "This field only accept numbers",
+    "Validate members did not validate that members contain numbers"
+  );
+  console.assert(
+    validateAmountOfMembers(3) === "",
+    "validateAmountOfMembers fail whit a amount valid"
   );
 }
 
-testValidateIntegrantes();
+testValidateAmountOfMembers();
